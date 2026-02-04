15e0ddecf8e41c286417dc15f31ecacd56269fbd
Cahit Attila <cahitatillab@gmail.com>
2026-02-05 02:24:47 +0300

    c done

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,35 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 02:38:03 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/02/04 23:33:15 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/02/05 02:16:30 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdarg.h>
 
-
 int	ft_printf(const char *format, ...)
 {
+	va_list args;
+	int i;
+	int count;
+	va_start(args, format);
 
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] == 'c')
+			ft_putchar(va_arg(args, int));
+		else if (format[i] == '%' && format[i + 1] == 's')
+			ft_putstr(va_arg(args, int));
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
+
+int main(void)
+{
+	ft_printf("%c", 'a');
 }
