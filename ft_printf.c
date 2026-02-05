40b57ba240847b7634d7168db69c708ce218dda4
Cahit Attila <cahitatillab@gmail.com>
2026-02-05 03:14:50 +0300

    s and c works all together

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 02:38:03 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/02/05 02:16:30 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/02/05 03:12:58 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,23 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 
 	i = 0;
+	count = 0;
 	while (format[i])
 	{
 		if (format[i] == '%' && format[i + 1] == 'c')
-			ft_putchar(va_arg(args, int));
+		{
+			count += ft_putchar(va_arg(args, int));
+			i++;
+		}
 		else if (format[i] == '%' && format[i + 1] == 's')
-			ft_putstr(va_arg(args, int));
+		{
+			count += ft_putstr(va_arg(args, char *));
+			i++;
+		}
+		else
+		{
+			count += ft_putchar(format[i]);
+		}
 		i++;
 	}
 
@@ -36,5 +47,5 @@ int	ft_printf(const char *format, ...)
 
 int main(void)
 {
-	ft_printf("%c", 'a');
+	ft_printf("%s\n %c", "merhaba", 'c');
 }
