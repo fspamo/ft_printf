87126018c4da12553874d9cf6da0c421cc61f0be
Cahit Atilla Bozkurt <cbozkurt@k2m09s07.42kocaeli.com.tr>
2026-02-06 16:34:35 +0300

    changed

--- ft_printf.h
+++ ft_printf.h
@@ -7,5 +7,7 @@ int		ft_putnbr(int n);
 int		ft_putchar(int c);
 int		ft_putstr(char *s);
 int		ft_print_hex(unsigned long val, size_t count);
+int		ft_lowercase_base(unsigned long val, size_t count);
+int		ft_uppercase(unsigned long val, size_t count);
 
 #endif
