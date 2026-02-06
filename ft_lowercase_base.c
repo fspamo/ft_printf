533cb79787e813e3b61fedc1c473bd0de291c04c
Cahit Atilla Bozkurt <cbozkurt@k2m09s07.42kocaeli.com.tr>
2026-02-06 16:32:56 +0300

    same as hex

--- ft_lowercase_base.c
+++ ft_lowercase_base.c
@@ -6,8 +6,24 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 15:36:07 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/02/06 15:36:21 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/02/06 16:04:49 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	lowercase_base()
+int	ft_lowercase_base(unsigned long	val, size_t count)
+{
+	char	*base;
+	size_t	holder;
+
+	base = "0123456789abcdef";
+	// count = write(1, "0x", 2);
+	if (count == (unsigned long)-1)
+		return (-1);
+	if (val > 16)
+		ft_print_hex(val / 16, count);
+	holder = count;
+	count += write(1, &base[val % 16], 1);
+	if (count == holder - 1)
+		return (-1);
+	return (count);
+}
