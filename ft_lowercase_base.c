b048dd45bd8009e3f91481258bf69ca28e48a3c2
Cahit Atilla Bozkurt <cbozkurt@k2m09s07.42kocaeli.com.tr>
2026-02-06 16:49:12 +0300

    typo fixed

--- ft_lowercase_base.c
+++ ft_lowercase_base.c
@@ -6,9 +6,12 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 15:36:07 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/02/06 16:04:49 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/02/06 16:42:52 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <stddef.h>
 
 int	ft_lowercase_base(unsigned long	val, size_t count)
 {
@@ -20,7 +23,7 @@ int	ft_lowercase_base(unsigned long	val, size_t count)
 	if (count == (unsigned long)-1)
 		return (-1);
 	if (val > 16)
-		ft_print_hex(val / 16, count);
+		ft_lowercase_base(val / 16, count);
 	holder = count;
 	count += write(1, &base[val % 16], 1);
 	if (count == holder - 1)
--- ft_uppercase_base.c
+++ ft_uppercase_base.c
@@ -6,11 +6,14 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 16:07:16 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/02/06 16:07:45 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/02/06 16:43:10 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_lowercase_base(unsigned long	val, size_t count)
+#include <unistd.h>
+#include <stddef.h>
+
+int	ft_uppercase_base(unsigned long	val, size_t count)
 {
 	char	*base;
 	size_t	holder;
@@ -20,7 +23,7 @@ int	ft_lowercase_base(unsigned long	val, size_t count)
 	if (count == (unsigned long)-1)
 		return (-1);
 	if (val > 16)
-		ft_print_hex(val / 16, count);
+		ft_uppercase_base(val / 16, count);
 	holder = count;
 	count += write(1, &base[val % 16], 1);
 	if (count == holder - 1)
