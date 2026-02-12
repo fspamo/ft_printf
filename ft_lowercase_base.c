31908af34fa695ccdb8e5c839b2bfc5294b1c41d
Cahit Atilla Bozkurt <cbozkurt@k1m01s01.42kocaeli.com.tr>
2026-02-12 16:53:27 +0300

    fixed norm

--- ft_lowercase_base.c
+++ ft_lowercase_base.c
@@ -13,15 +13,16 @@
 #include <unistd.h>
 #include <stddef.h>
 
-int ft_lowercase_base(unsigned long val, size_t *count)
+int	ft_lowercase_base(unsigned long val, size_t *count)
 {
-    char *base = "0123456789abcdef";
+	char	*base;
 
-    if (val >= 16)
-        if (ft_lowercase_base(val / 16, count) == -1)
-            return (-1);
-    if (write(1, &base[val % 16], 1) == -1)
-        return (-1);
-    (*count)++;
-    return (0);
+	base = "0123456789abcdef";
+	if (val >= 16)
+		if (ft_lowercase_base(val / 16, count) == -1)
+			return (-1);
+	if (write(1, &base[val % 16], 1) == -1)
+		return (-1);
+	(*count)++;
+	return (0);
 }
