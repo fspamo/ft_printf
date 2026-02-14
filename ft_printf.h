ca1a176953865587dee2e1bbd81b7732f2c7aa74
cbozkurt <cahitatillab@gmail.com>
2026-02-14 07:13:04 +0300

    non-used function removed

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 19:28:05 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/02/12 19:28:20 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/02/14 07:03:14 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int		ft_putnbr(int n, int *count);
 int		ft_putchar(char c, int *count);
 int		ft_putstr(char *s, int *count);
 int		ft_print_hex(unsigned long val, int *count);
-int		ft_lowercase_base(unsigned long val, size_t *count);
 int		ft_print_u(unsigned int n, int *count);
 void	ft_uppercase_base(unsigned long val, int *count);
 
